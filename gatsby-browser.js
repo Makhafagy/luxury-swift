8dae78c1f95a86ca82d401be7ecbbde8f352b46f
hussaa <mah.hussaina@gmail.com>
2024-06-08 03:40:33 -0400

    yarn

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,4 @@
 import "@mantine/core/styles.css";
-
-
-
 import "./src/styles/global.css"
-import "bootstrap-icons/font/bootstrap-icons.css"
 import "./src/styles/carousel.css"
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
